a099b9ad68a8a65d5a443e861e16a17a9398d86f
Eric Newton <ericnewton76@gmail.com>
2015-10-21 19:25:32 -0400

    [build] fix nuget calling, because on appveyor its in the path

--- build-nuget.cmd
+++ build-nuget.cmd
@@ -2,12 +2,14 @@
 setlocal
 
 REM initialization
-set NUGET_EXE=..\packages\Nuget.Commandline.2.8.6\tools\nuget.exe
+set NUGET_EXE=%~dp0\packages\Nuget.Commandline.2.8.6\tools\nuget.exe
 
 REM appveyor
 if not "%APPVEYOR_BUILD_VERSION%" == "" set BUILD_VERSION=%APPVEYOR_BUILD_VERSION%
+if not "%APPVEYOR_BUILD_VERSION%" == "" set NUGET_EXE=nuget
 
 REM checks
+echo %NUGET_EXE%
 if "%BUILD_VERSION%" == "" echo Missing BUILD_VERSION & goto :END
 
 if "%1" == "--after-build" goto :SKIP_BUILD
