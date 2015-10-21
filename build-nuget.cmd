88bb8d7304d4b94924fe988891f0490af31ca3b4
Eric Newton <ericnewton76@gmail.com>
2015-10-21 18:52:30 -0400

    added --after-build parameter for running within appveyor

--- build-nuget.cmd
+++ build-nuget.cmd
@@ -4,11 +4,14 @@ setlocal
 set NUGET_EXE=..\packages\Nuget.Commandline.2.8.6\tools\nuget.exe
 if "%BUILD_VERSION%" == "" echo Missing BUILD_VERSION & goto :END
 
+if "%1" == "--after-build" goto :SKIP_BUILD
+
 echo.
-if not "%1" == "--no-msbuild" call .\build.cmd & if errorlevel 1 goto :END
+if "%1" == "--no-msbuild" goto :SKIP_BUILD
+call .\build.cmd & if errorlevel 1 goto :END
 shift
 
-
+:SKIP_BUILD
 pushd Build
 %NUGET_EXE% pack ..\DataUri.nuspec -version %BUILD_VERSION%
 
