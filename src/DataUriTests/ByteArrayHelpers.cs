66d5bea794175c97ae4846903eded38e4e310c91
Eric Newton <ericnewton76@gmail.com>
2015-10-22 13:54:47 -0400

    added ByteArrayHelpers to project

---
+++ src/DataUriTests/ByteArrayHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataUriTests
+{
+	internal class ByteArrayHelpers
+	{
+
+		public static byte[] HexStringToByteArray(string hexString) //http://stackoverflow.com/a/8235530/323456
+		{
+			if(hexString.Length % 2 != 0)
+			{
+				throw new ArgumentException("The hex values string cannot have an odd number of digits.");
+			}
+
+			byte[] HexAsBytes = new byte[hexString.Length / 2];
+			for(int index = 0; index < HexAsBytes.Length; index++)
+			{
+				string byteValue = hexString.Substring(index * 2, 2);
+				HexAsBytes[index] = byte.Parse(byteValue, System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.InvariantCulture);
+			}
+
+			return HexAsBytes;
+		}
+
+	}
+}
--- src/DataUriTests/DataUriTests.cs
+++ src/DataUriTests/DataUriTests.cs
@@ -35,24 +35,5 @@ namespace DataUriTests
 			});
 		}
 
-		#region test helpers
-		public static byte[] HexStringToByteArray(string hexString) //http://stackoverflow.com/a/8235530/323456
-		{
-			if (hexString.Length % 2 != 0)
-			{
-				throw new ArgumentException("The hex values string cannot have an odd number of digits.");
-			}
-
-			byte[] HexAsBytes = new byte[hexString.Length / 2];
-			for (int index = 0; index < HexAsBytes.Length; index++)
-			{
-				string byteValue = hexString.Substring(index * 2, 2);
-				HexAsBytes[index] = byte.Parse(byteValue, System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.InvariantCulture);
-			}
-
-			return HexAsBytes;
-		}
-		#endregion
-
 	}
 }
