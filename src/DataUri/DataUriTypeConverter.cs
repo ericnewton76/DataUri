890fcf8c106606c985f41d4a5c72cffaf3b40b59
Eric Newton <ericnewton76@gmail.com>
2015-10-22 12:23:01 -0400

    created DataUriTypeConverter and associated tests

---
+++ src/DataUri/DataUriTypeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Net
+{
+	public class DataUriTypeConverter : System.ComponentModel.TypeConverter
+	{
+
+		public override bool CanConvertFrom(ComponentModel.ITypeDescriptorContext context, Type sourceType)
+		{
+			if(sourceType == typeof(string))
+				return true;
+
+			return base.CanConvertFrom(context, sourceType);
+		}
+
+		public override object ConvertFrom(ComponentModel.ITypeDescriptorContext context, Globalization.CultureInfo culture, object value)
+		{
+			if(value is string)
+			{
+				return DataUri.Parse((string)value);
+			}
+			return base.ConvertFrom(context, culture, value);
+		}
+	}
+}
---
+++ src/DataUriTests/DataUriTypeConverterTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using System.Net;
+
+namespace DataUriTests
+{
+	[TestFixture]
+	public class DataUriTypeConverterTests
+	{
+		[Test]
+		public void CanConvertFrom_String_Returns_True()
+		{
+			//arrange
+			DataUriTypeConverter c = Create_DataUriTypeConverter();
+
+			//act
+			var actual = c.CanConvertFrom(typeof(string));
+
+			//assert
+			Assert.That(actual, Is.EqualTo(true));
+
+		}
+
+		[Test]
+		public void ConvertTo_DataUri()
+		{
+			//arrange
+			var value = "data:image/jpeg;base64,QUJDREVGRw==";
+
+			var expected_DataUri = new DataUri("image/jpeg", new byte[] { 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47 });
+
+			//act
+			var typeconverter = Create_DataUriTypeConverter();
+			var actual_DataUri = (DataUri)typeconverter.ConvertFrom(value);
+
+			//assert
+			Assert.That(actual_DataUri, Is.Not.Null);
+			Assert.That(actual_DataUri, Is.TypeOf<System.Net.DataUri>());
+			//Assert.That(actual_DataUri.MediaType, Is.EqualTo("image/jpeg"));
+			//Assert.That(actual_DataUri.Bytes, Is.EqualTo(expected_DataUri.Bytes));
+		}
+
+		DataUriTypeConverter Create_DataUriTypeConverter()
+		{
+			return new DataUriTypeConverter();
+		}
+	}
+}
